55b198fd2ad0f3c346a3a26f417caac611087367
Beatriz Silva Ramos <beatrizr@br.ibm.com>
2016-08-04 00:03:57 -0300

    improvements

--- www/disponibilidade/disponibilidade.controller.js
+++ www/disponibilidade/disponibilidade.controller.js
@@ -4,12 +4,33 @@
         .module('disponibilidade.controller', [])
         .controller('DisponibilidadeCtrl', DisponibilidadeCtrl);
 
-    DisponibilidadeCtrl.$inject = ['$rootScope', '$scope', '$state'];
-    function DisponibilidadeCtrl($rootScope, $scope, $state) {
+    DisponibilidadeCtrl.$inject = ['$rootScope', '$scope', '$state', 'ReservaService', '$ionicLoading'];
+    function DisponibilidadeCtrl($rootScope, $scope, $state, ReservaService, $ionicLoading) {
 
+      $scope.categoria = false;
+      $scope.disp = [];
 
-      $scope.backDisp= function() {
-        $state.go('main');
+      $scope.showCategoria = function(quantidade) {
+        $ionicLoading.show();
+        $scope.categoria = false;
+        if(quantidade !== '' && quantidade !== undefined && quantidade !== null)
+        ReservaService.categorias(quantidade)
+          .then(
+            function(data) {
+                console.log(data);
+                if (data.length > 0) {
+                  $scope.categoria = true;
+                  $scope.listcategorias = data;
+                }
+
+                $ionicLoading.hide();
+
+            },
+            function(error) {
+                $scope.showAlert("Tente novamente");
+                $ionicLoading.hide();
+            }
+          );
       }
 
     };
